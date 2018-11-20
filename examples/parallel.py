bea7512e58ff750b687567b14eb695d527ef4f84
Baha ID <gitkraken@baha.web.id>
2018-11-20 22:51:08 +0700

    menghilangkan batas jumlah proses

--- examples/parallel.py
+++ examples/parallel.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     # perform on each input. For example...
     inputs = range(10000000)
     #  removing processes argument makes the code run on all available cores
-    pool = mp.Pool(processes=4)
+    # pool = mp.Pool(processes=4)
+    pool = mp.Pool()
     results = pool.map(processInput, inputs)
     print(results)
