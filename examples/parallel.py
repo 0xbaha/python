887feab5e020a758c4fffe226ed2bdbff54a682e
Baha ID <gitkraken@baha.web.id>
2018-11-20 22:48:21 +0700

    init parallel.py

---
+++ examples/parallel.py
@@ -0,0 +1,16 @@
+# https://stackoverflow.com/questions/33787678/writing-a-parallel-loop
+
+import multiprocessing as mp
+
+def processInput(i):
+        return i * i
+
+if __name__ == '__main__':
+
+    # what are your inputs, and what operation do you want to
+    # perform on each input. For example...
+    inputs = range(10000000)
+    #  removing processes argument makes the code run on all available cores
+    pool = mp.Pool(processes=4)
+    results = pool.map(processInput, inputs)
+    print(results)
