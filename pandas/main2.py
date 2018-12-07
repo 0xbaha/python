51e1a4cd2e22ece6952602a678168f9cc8211e3d
Baha ID <gitkraken@baha.web.id>
2018-12-07 07:39:43 +0700

    update pandas utk seleksi dan sort saat import

--- pandas/main2.py
+++ pandas/main2.py
@@ -1,5 +1,6 @@
 # https://chrisalbon.com/python/data_wrangling/pandas_dataframe_importing_csv/
 # https://stackoverflow.com/questions/32011359/convert-categorical-data-in-pandas-dataframe
+# https://stackoverflow.com/questions/48641632/extracting-specific-columns-from-pandas-dataframe
 import os
 import pandas as pd
 import numpy as np
@@ -10,7 +11,10 @@ dir_input = 'input'
 filename = 'dataset2.csv'
 path_file = os.path.join(dir_current,dir_input,filename)
 id_col = 0 # Index column
-df = pd.read_csv(path_file,header=None)
+dataset = pd.read_csv(path_file,header=None)
+df = pd.DataFrame(dataset)
+cols = [4,5,6,7]
+df = df[df.columns[cols]]
 print('df:\n',df)
 print('df.dtypes:\n',df.dtypes)
 cat_columns = df.select_dtypes(['object']).columns
