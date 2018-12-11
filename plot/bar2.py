faead5ded8d6a9db6556de071f2eb4d201d925fe
Baha ID <gitkraken@baha.web.id>
2018-12-11 22:20:00 +0700

    init bar2.py
    membuat grafik bar dengan warna tertentu pada nama yg telah ditentukan

---
+++ plot/bar2.py
@@ -0,0 +1,17 @@
+# https://stackoverflow.com/questions/18973404/setting-different-bar-color-in-matplotlib-python
+import matplotlib.pyplot as plt
+import operator
+from functools import reduce
+
+nama = ['ee','bb','dd','hh','aa']
+nilai = [44,77,33,88,55]
+
+# menentukan warna pada nama tertentu
+nama_terpilih = 'hh'
+for i,j in enumerate(nama):
+    if j == nama_terpilih:
+        kolom_terpilih = i
+
+barlist = plt.bar(nama,nilai)
+barlist[kolom_terpilih].set_color('r')
+plt.show()
