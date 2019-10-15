52a20a2030450a58eac3f910431efb2651c03630
Baha ID <gitkraken@baha.web.id>
2019-10-15 09:22:20 +0700

    init if-simple1.py

---
+++ if/if-simple1.py
@@ -0,0 +1,19 @@
+# https://www.programiz.com/python-programming/if-elif-else
+
+# If the number is positive, we print an appropriate message
+
+num = 3
+if num > 0:
+    print(num, "is a positive number.")
+print("This is always printed.")
+
+num = -1
+if num > 0:
+    print(num, "is a positive number.")
+print("This is also always printed.")
+
+# OUTPUT #
+# > python if-simple1.py
+# (3, 'is a positive number.')
+# This is always printed.
+# This is also always printed.
