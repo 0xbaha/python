26b10d3dd0a76dc2cf739ed2392016371b115fa8
Baha ID <gitkraken@baha.web.id>
2019-10-15 09:32:04 +0700

    init if-simple3.py

---
+++ if/if-simple3.py
@@ -0,0 +1,10 @@
+# https://www.programiz.com/python-programming/examples/positive-negative-zero
+
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
